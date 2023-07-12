031e080c5616ae8080ac2b33b60fd249507ffb25
jddios837 <jddios837@gmail.com>
2023-07-11 20:44:18 -0500

    added Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSidenavModule} from "@angular/material/sidenav";
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoremIpsumComponent } from './lorem-ipsum/lorem-ipsum.component';
 import { ButtonComponent } from './button/button.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import {MatListModule} from "@angular/material/list";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
@@ -16,7 +21,11 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
